593cd6929080cd0d0de3647619afebc34c57f78e
Tyler Menezes <tylermenezes@codeday.org>
2021-06-13 11:40:36 -0400

    Add query log

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import http from 'http';
 import { ApolloServer } from 'apollo-server-express';
 import { graphqlUploadExpress } from 'graphql-upload';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
+import syslog from 'syslog-client';
 import { default as WebSocket } from 'ws';
 import ws from 'ws';
 import { execute, subscribe } from 'graphql';
@@ -19,6 +20,11 @@ import { addAuthContext, addWsAuthContext } from './auth';
 import { weave } from './schema';
 
 const port = process.env.PORT || 4000;
+
+const logger = syslog.createClient(process.env.SYSLOG_HOST, {
+  port: process.env.SYSLOG_PORT,
+  syslogHostname: `gql`,
+});
 
 export default async () => {
   globalThis.WebSocket = WebSocket;
@@ -60,6 +66,49 @@ export default async () => {
       endpoint: '/',
       subscriptionEndpoint: '/subscriptions',
     },
+    plugins: [
+      {
+        requestDidStart({ schemaHash, context: { req }, ...rest }) {
+          const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+          return {
+            validationDidStart({ source, queryHash }) {
+              logger.log([
+                queryHash,
+                ip,
+                `QUERY`,
+                `"${source?.split(`\n`).filter((line) => Boolean(line.trim())).join(' ').replace(/"/g, `'`)}"`,
+                schemaHash,
+              ].join(' '));
+              return (errs) => {
+                (errs || []).forEach((e) => {
+                  logger.log([
+                    queryHash,
+                    ip,
+                    `PARSE_ERROR`,
+                    `"${e?.toString().split(`\n`).join('; ')}"`,
+                    `"${e?.stack?.split(`\n`).join('; ')}"`
+                  ].join(' '));
+                });
+              }
+            },
+
+            didEncounterErrors({ queryHash, errors }) {
+              console.log(errors);
+              (errors || []).forEach((e) => {
+                logger.log([
+                  queryHash,
+                  ip,
+                  `EXEC_ERROR`,
+                  `"${e?.toString().split(`\n`).join('; ')}"`,
+                  `"${e?.stack?.split(`\n`).join('; ')}"`
+                ].join(' '));
+              });
+            }
+          }
+        },
+      }
+    ],
     context: ({ req }) => ({
       headers: req?.headers,
       req,
